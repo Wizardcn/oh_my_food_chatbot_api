520108ec40e5313f861e44af9599cae58b01d236
TONY <supawat125947@gmail.com>
2022-08-20 01:20:07 +0700

    fix bug

--- schemas/carts_schema.py
+++ schemas/carts_schema.py
@@ -1,7 +1,7 @@
 def cart_serializer(cart) -> dict:
     return {
-        "customer_id": str(cart["customer_id"]),
-        "cart": dict(cart["cart"])
+        "customer_id": cart["customer_id"],
+        "cart": cart["cart"]
     }
     
 def carts_serializer(carts) -> list:
