109145d4a4a8597491a6d109dee8147f10fd36c4
TONY <supawat125947@gmail.com>
2022-08-20 00:55:45 +0700

    create functons to connect with db

---
+++ repositories/cart_repo.py
@@ -0,0 +1,33 @@
+from dbconnector import *
+from schemas import *
+from models import *
+import pymongo
+
+dse6g1customer_db = MongoConnector.connect()
+carts_col = dse6g1customer_db["carts"]
+
+def query_cart(customer_id: str):
+    try:
+        carts = carts_serializer(carts_col.find({"customer_id": customer_id}))
+        # print(carts)
+        if len(carts) == 0:
+            return {404: "Cart with the customer_id is not available"}
+        else:
+            return {200: carts[0]}
+    except pymongo.errors.ConnectionFailure:
+        return {520: "Fail to connect Database"}    
+
+
+def update_cart(customer_id: str, cart: Cart):
+    try:
+        result = cart_serializer(carts_col.find_one_and_update({"customer_id": customer_id}, {
+            "$set": dict(cart)
+        }))
+        if result == None:
+            return {404: "Fail to update document"}
+        else:
+            return query_cart(customer_id)
+
+    except pymongo.errors.ConnectionFailure:
+        return {520: "Fail to connect database"}
+        
--- repositories/food_repo.py
+++ repositories/food_repo.py
@@ -1,18 +1,29 @@
-from dbconnector import Mongoatlas
+import pymongo
+from dbconnector import *
+from schemas import *
 
-# เชื่อมต่อกับ blind app database
-foods_col = Mongoatlas(collection="foods")
+
+dse6g1customer_db = MongoConnector.connect()
+foods_col = dse6g1customer_db["foods"]
 
 def query_with_food_id(food_id_list: list):
     try:
-        foods = foods_col.find({"food_id": {
+        foods = foods_serializer(foods_col.find({"food_id": {
             "$in": food_id_list
-        }})["documents"]
+        }}))
         if len(foods) == 0:
             return {404: "Foods with these food_id is not available"}
         else:
             return {200: foods}
-    except:
-        return {520: "Fail to connnect Database"}
-    
-        
+    except pymongo.errors.ConnectionFailure:
+        return {520: "Fail to connect Database"}
+
+def query_with_food_name(food_name: str):
+    try:
+        foods = foods_serializer(foods_col.find({"food_name": food_name}))
+        if len(foods) == 0:
+            return {404: "Food with the food_name is not available"}
+        else:
+            return {200: foods[0]}
+    except pymongo.errors.ConnectionFailure:
+        return {520: "Fail to connect Database"}
