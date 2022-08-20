c271da3d7d1c5a746ccf73242c3a481612d635b6
TONY <supawat125947@gmail.com>
2022-08-20 13:50:18 +0700

    change http verb

--- services/cart_service.py
+++ services/cart_service.py
@@ -8,24 +8,25 @@ from utils import *
 
 cart_router = APIRouter(tags=["carts"])
 
-@cart_router.put("/", status_code=status.HTTP_200_OK, 
-                            responses={
-                                200: {
-                                    "model": Cart,
-                                    "description": "Cart Was Updated"
-                                },
-                                520: {
-                                    'model': DatabaseError,
-                                    'description': 'Database Connection Error'
-                                }
-                            })
+
+@cart_router.post("/", status_code=status.HTTP_200_OK,
+                  responses={
+                      200: {
+                          "model": Cart,
+                          "description": "Cart Was Updated"
+                      },
+                      520: {
+                          'model': DatabaseError,
+                          'description': 'Database Connection Error'
+                      }
+                  })
 async def add_food_to_cart(body: AddFoodRequestBody):
-    
+
     body = dict(body)
     customer_id = str(body["customer_id"])
     food_name = str(body["food_name"])
     amount = int(body["amount"])
-    
+
     result = add_food_on_customer_cart(customer_id, food_name, amount)
 
     if 200 in result:
@@ -33,22 +34,22 @@ async def add_food_to_cart(body: AddFoodRequestBody):
     else:
         error_key = list(result.keys())[0]
         raise HTTPException(status_code=error_key, detail=result[error_key])
-    
-    
-@cart_router.get("/{customer_id}", status_code=status.HTTP_200_OK, 
-                         responses={
-                             200: {
-                                 "model": CartFlex,
-                             },
-                             520: {
-                                'model': DatabaseError,
-                                'description': 'Database Connection Error'
-                             }
-                         })
+
+
+@cart_router.get("/{customer_id}", status_code=status.HTTP_200_OK,
+                 responses={
+                     200: {
+                         "model": CartFlex,
+                     },
+                     520: {
+                         'model': DatabaseError,
+                         'description': 'Database Connection Error'
+                     }
+                 })
 async def get_customer_cart_flex(customer_id: str):
-    
+
     result = query_cart(customer_id)
-    
+
     if 200 in result:
         cart = result[200]
         cart_data = cart["cart"]
