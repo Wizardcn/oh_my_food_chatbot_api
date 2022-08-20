b5362756c5bb6700eb4a2a0695cd1eca6fc327ba
TONY <supawat125947@gmail.com>
2022-08-20 13:25:55 +0700

    create service for send flex with category

--- services/food_service.py
+++ services/food_service.py
@@ -17,7 +17,7 @@ food_router = APIRouter(tags=["foods"])
                                 'description': 'Database Connection Error'
                              }
                          })
-async def get_foods_flexs(food_id_request_body: FoodIdRequestBody):
+async def get_foods_flexs_by_food_id(food_id_request_body: FoodIdRequestBody):
     
     food_id_list = dict(food_id_request_body)["food_id_list"]
     
@@ -31,4 +31,29 @@ async def get_foods_flexs(food_id_request_body: FoodIdRequestBody):
     else:
         error_key = list(result.keys())[0]
         raise HTTPException(status_code=error_key, detail=result[error_key])
+    
+    
+@food_router.get("/{category}", status_code=status.HTTP_200_OK, responses={
+                             200: {
+                                 "model": FoodFlex,
+                             },
+                             520: {
+                                'model': DatabaseError,
+                                'description': 'Database Connection Error'
+                             }
+                         })
+async def get_foods_flexs_by_category(category: str):
+    
+    result = query_with_food_category(category)
+    
+    if 200 in result:
+        food_data = result[200]
+        foods_flex = gen_foods_flexs(food_data)
+        return botnoi_payload(foods_flex)
+    elif 404 in result:
+        return botnoi_payload(text_message(["ไม่พบสินค้าในระบบนะครับ"]))
+    else:
+        error_key = list(result.keys())[0]
+        raise HTTPException(status_code=error_key, detail=result[error_key])
+    
     
